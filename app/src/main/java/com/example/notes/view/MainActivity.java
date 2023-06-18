951f01c7b024186dd7428a26a6846cdaa261c9ae
abubakrkh <abu.khuvai@gmail.com>
2023-06-18 22:40:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notes/view/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.notes.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.notes.R;
+import com.example.notes.view.SecondActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // создание полей
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // присваивание id полям
+        imageButton = findViewById(R.id.imageButton);
+
+        // обработка нажатия кнопки
+        imageButton.setOnClickListener(listener);
+    }
+
+    // создание слушателя
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            // переключение на новую активность
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
+
+        }
+    };
+}
---
+++ app/src/main/java/com/example/notes/view/SecondActivity.java
@@ -0,0 +1,90 @@
+package com.example.notes.view;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notes.R;
+import com.example.notes.modell.Notebook;
+import com.example.notes.viewmodell.Adapter;
+import com.example.notes.viewmodell.DatabaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
+
+    // создание полей
+    private RecyclerView recyclerView; // поле для списка RecyclerView
+    private FloatingActionButton fabAdd; // поле для кнопки добавить новую заметку
+    private Button btn;
+
+    private List<Notebook> notesList; // поле для контейнера списка заметок
+
+    private DatabaseHelper database; // поле работы с БД
+    private Adapter adapter; // поле для адаптера
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // присваивание id полям
+        recyclerView = findViewById(R.id.recycler_list);
+        fabAdd = findViewById(R.id.fabAdd);
+        btn = findViewById(R.id.btn);
+
+        notesList = new ArrayList<>(); // выделение памяти и задание типа контейнера для списка заметок
+        database = new DatabaseHelper(this); // выделение памяти и задание текущего контекста работы с БД
+
+        // считывание данных из БД и запись их в коллекцию notesList
+        fetchAllNotes();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // задание структуры вывода данных в recyclerView
+        adapter = new Adapter(this, SecondActivity.this, notesList); // инициализация адаптера и передача в рего данных из БД
+        recyclerView.setAdapter(adapter); // передача в recyclerView адаптер
+
+        // обработка нажатия кнопки создания новой заметки
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // переключение на новую активность
+                startActivity(new Intent(SecondActivity.this, AddNotesActivity.class));
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper database = new DatabaseHelper(SecondActivity.this); // создание объекта БД в текущей активности
+                database.deleteAllNotes();
+            }
+        });
+    }
+
+
+    // метод считывания из БД всех записей
+    public void fetchAllNotes(){
+        // чтение БД и запись данных в курсор
+        Cursor cursor = database.readNotes();
+
+        if (cursor.getCount() == 0) { // если данные отсутствую, то вывод на экран об этом тоста
+            Toast.makeText(this, "Заметок нет", Toast.LENGTH_SHORT).show();
+        } else { // иначе помещение их в контейнер данных notesList
+            while (cursor.moveToNext()){
+                // помещение в контейнер notesList из курсора данных
+                notesList.add(new Notebook(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+            }
+        }
+    }
+}
